496130d1c35c0e231758bf3ac6381e1d301c4374
Victor  Sanchez <sanchezvs2004@gmail.com>
2025-11-10 21:56:15 -0500

    weapon system

--- Assets/Scripts/ScriptableObjectsScripts/BatWeaponSettings.cs
+++ Assets/Scripts/ScriptableObjectsScripts/BatWeaponSettings.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BatWeaponSettings", menuName = "Scriptable Objects/BatWeaponSettings")]
 public class BatWeaponSettings : MeleeWeaponSettings
 {
-    
+    public float swingRadius;
+    public float swingRange = 1.5f;
+    public float swingDuration = 0.3f;
     public float timeBetweenSwings = 0.5f;
     public float AttackDelay;
 }
--- Assets/Scripts/ScriptableObjectsScripts/KnifeWeaponSettings.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "KnifeWeaponSettings", menuName = "Scriptable Objects/KnifeWeaponSettings")]
-public class KnifeWeaponSettings : MeleeWeaponSettings
-{
-    public float timeBetweenSlash, AttackDelay;
-}
