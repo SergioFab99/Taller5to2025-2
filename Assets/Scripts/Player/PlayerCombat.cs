e2694b957d9f43aa8c48bdffb405cb309e6df5cf
Akinoid <130516420+Akinoid@users.noreply.github.com>
2025-12-02 18:24:01 -0500

    update

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -193,10 +193,10 @@ public class PlayerCombat : CombatBase
             _state.isBlocking = false;
             if (_state.playerActionState == PlayerActionState.Blocking)
                 _state.playerActionState = PlayerActionState.Normal;
+            SyncCombatHitReceivers(false);
         }
-        //cambiar el metodo de recivir el componente dependiendo de su lugar UWwU
-        var recv = GetComponentInChildren<CombatHitReceiver>();
-            if (recv != null) recv.isBlocking = false;
+        
+        SyncCombatHitReceivers(_state.isBlocking);
         
         if (requestDodge && !_isDodging)
         {
@@ -244,6 +244,16 @@ public class PlayerCombat : CombatBase
                 number = 1;
                 OnAttack?.Invoke(number);
             }
+            else if((currentWeapon as BrassKnuklesWeapon).currentHand == CombatHand.None || (currentWeapon as BrassKnuklesWeapon).currentHand ==  CombatHand.Left)
+            {
+                number = 0;
+                OnAttack?.Invoke(number);
+            }
+            else if((currentWeapon as BrassKnuklesWeapon).currentHand == CombatHand.Right)
+            {
+                number = 1;
+                OnAttack?.Invoke(number);
+            }
            
                 
         }
@@ -281,10 +291,32 @@ public class PlayerCombat : CombatBase
         _state.isBlocking = true;
         _state.playerActionState = PlayerActionState.Blocking;
         Debug.Log("Blocking");
+        SyncCombatHitReceivers(true);
+    }
 
-        // LO MISMO que en el otro. cambiuar dependiendo de su lugar UwU
-        var recv = GetComponentInChildren<CombatHitReceiver>();
-        if (recv != null) recv.isBlocking = true;
+    
+    private void SyncCombatHitReceivers(bool value)
+    {
+        var seen = new System.Collections.Generic.HashSet<CombatHitReceiver>();
+        var parents = GetComponentsInParent<CombatHitReceiver>(true);
+        foreach (var p in parents)
+        {
+            if (p != null && !seen.Contains(p))
+            {
+                p.isBlocking = value;
+                seen.Add(p);
+            }
+        }
+
+        var children = GetComponentsInChildren<CombatHitReceiver>(true);
+        foreach (var c in children)
+        {
+            if (c != null && !seen.Contains(c))
+            {
+                c.isBlocking = value;
+                seen.Add(c);
+            }
+        }
     }
 
     public void LinkWeapon(GameObject obj)
